f06626aa1bc5651d1b16f494316fb2ccabf11364
Steen Larsen <stel@yousee.dk>
2013-10-10 16:26:24 +0200

    static session works

--- src/hornetqtest/core.clj
+++ src/hornetqtest/core.clj
@@ -1,15 +1,23 @@
 (ns hornetqtest.core
   (:import (org.hornetq.api.core.client HornetQClient MessageHandler)
-           (org.hornetq.api.core DiscoveryGroupConfiguration)
-           (org.hornetq.api.core UDPBroadcastGroupConfiguration)))
+           (org.hornetq.api.core UDPBroadcastGroupConfiguration TransportConfiguration DiscoveryGroupConfiguration)
+           (org.hornetq.core.remoting.impl.netty NettyConnectorFactory)))
 
 (def group-address "231.7.7.7")
 
 (def group-port 9876)
 
-(def refresh-timeout 5000)
+(def refresh-timeout 10000)
 
-(def discovery-initial-wait-timeout 5000)
+(def discovery-initial-wait-timeout 10000)
+
+(def pico5 "10.146.68.45")
+
+(def pico6 "10.146.68.46")
+
+(def server1 {"host" pico5 "port" 5455})
+
+(def server2 {"host" pico6 "port" 5455})
 
 ;;TODO figure out exactly how windowsize corresponds to ackbatchsize. Maybe ackbatchsize should just be 0
 (defn create-session [username passwd auto-commit-sends auto-commit-acks pre-ack batch-size]
@@ -17,8 +25,17 @@
         factory (.createSessionFactory locator)]
     (. factory (createSession username passwd false auto-commit-sends auto-commit-acks pre-ack batch-size))))
 
+(defn create-static-session [servers username passwd auto-commit-sends auto-commit-acks pre-ack batch-size]
+  (let [configs (map #(TransportConfiguration. (.getName (.getClass (NettyConnectorFactory.))) %) servers)
+        locator (HornetQClient/createServerLocatorWithHA (into-array TransportConfiguration configs))
+        factory (.createSessionFactory locator)]
+    (. factory (createSession username passwd false auto-commit-sends auto-commit-acks pre-ack batch-size))))
+
 (defn default-session []
   (create-session "guest" "guest" true true false 1))
+
+(defn default-static-session []
+  (create-static-session [server1 server2] "guest" "guest" true true false 1))
 
 (defn create-queue [session address queue durable]
   (. session (createQueue address queue durable)))
@@ -64,7 +81,7 @@
     (prn "M" (.readString (.getBodyBuffer m)))))
 
 (defn receive-session [qn]
-  (let [s (default-session)
+  (let [s (default-static-session)
         q (create-temp-queue s qn qn)
         p (get-producer s qn)
         m (create-message s "Hej" false)]
--- test/hornetqtest/core_test.clj
+++ test/hornetqtest/core_test.clj
@@ -6,4 +6,8 @@
 
 (def pico6 "10.146.68.46")
 
+(def server1 {:host pico5 :port 9876})
+
+(def server2 {:host pico6 :port 9876})
+
 (expect #"foo" (str "boo" "foo" "ar"))
